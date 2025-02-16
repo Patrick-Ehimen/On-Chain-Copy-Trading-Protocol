39d8a6d3f7c67f927540026d268ea412e5e98067
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2025-02-16 22:35:45 +0100

    Update lib.rs

--- programs/copy_trading/src/lib.rs
+++ programs/copy_trading/src/lib.rs
@@ -1,16 +1,36 @@
 use anchor_lang::prelude::*;
 
-declare_id!("8sxRmfq8W6gaH9FVH1H5oZzm6S9GE6hRkerhT6jQQvp5");
+declare_id!("BoXJetaQMDEpgBpsqpoiGbKP2gGEDcvUkQPZNdJHqE65");
 
-#[program]
-pub mod copy_trading {
+const MAX_TRADERS: usize = 100;
+
+pub mod master_traders {
     use super::*;
-
+    
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+        ctx.accounts.trader_list.admin = ctx.accounts.admin.key();
+        ctx.accounts.trader_list.traders = Vec::new();
         Ok(())
     }
 }
 
+// Account structure for initialization
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(
+        init, 
+        payer = admin, 
+        space = 8 + 32 + 4 + (32 * MAX_TRADERS)
+    )]
+    pub trader_list: Account<'info, TraderList>,
+    #[account(mut)]
+    pub admin: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+// Data structure for storing traders
+#[account]
+pub struct TraderList {
+    pub admin: Pubkey,
+    pub traders: Vec<Pubkey>,
+}
